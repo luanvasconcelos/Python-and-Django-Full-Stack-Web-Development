75acd1c240b93ff1a7997c1520a3d598606dbca8
Luan Vasconcelos <luan-vasconcelos@hotmail.com>
2019-02-16 01:00:09 -0200

    Delete views.py

--- ProTwo/appTwo/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def help(request):
-    helpdict ={'help_insert':'Help page'}
-    return render(request, 'apptwo/help.html', context=helpdict)
